085f39ab871e052a452409ad7db972bec354faee
Matty-10 <matias.ezequiel.2004@hotmail.com>
2023-07-27 22:44:00 -0300

    Prueba de commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var cantidadDeGatos = 1;
+
+  do {
+    if(cantidadDeGatos<10){
+    console.log('Gato #' + cantidadDeGatos + ': 😺')
+    cantidadDeGatos++
+    console.log('Gato #' + cantidadDeGatos + ': 😸')
+    cantidadDeGatos++
+    console.log('Gato #' + cantidadDeGatos + ': 😹')
+    cantidadDeGatos++
+    }
+   else{
+   (console.log('Gato #' + cantidadDeGatos + ': 😺'))
+break
+}}
+    while(cantidadDeGatos<11)
